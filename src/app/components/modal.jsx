bcd8d234f53737c7418c0e3d839036b052f289d0
Isaac Sarte <sarteisaac09@gmail.com>
2024-01-17 18:35:31 +0800

    installed cn from classnames

--- src/app/components/modal.jsx
+++ src/app/components/modal.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useUsers } from '../hooks/users/use-users';
+import cn from 'classnames';
 
 const Modal = ({ isModalOpen, setIsModalOpen, user, setUser }) => {
     const [userId, setUserId] = useState('');
@@ -34,12 +35,13 @@ const Modal = ({ isModalOpen, setIsModalOpen, user, setUser }) => {
     // classnames here are based on inspecting elements on Storybook
 
     return (
-        <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-50">
-            <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
+        <div className="fixed w-full h-full inset-0 bg-gray-700 bg-opacity-50 overflow-y-auto z-50">
+            <div className="relative bg-white top-20 w-96 mx-auto p-5 border shadow-lg rounded-md">
                 <div className="flex flex-col gap-2">
                     <div className="flex flex-col">
                         <label className="text-base font-medium" htmlFor="">
                             User ID
+                            <span className="text-red-500">*</span>
                         </label>
                         <input
                             className="w-full border border-gray-300 px-3 py-2 rounded-md shadow-sm focus:outline-none focus:border-purple-500 focus:ring-1 focus:ring-purple-500"
@@ -48,12 +50,12 @@ const Modal = ({ isModalOpen, setIsModalOpen, user, setUser }) => {
                             onChange={(e) => setUserId(e.target.value)}
                         />
                         {userIdError && (
-                            <span className="text-sm text-red-500">User ID Required</span>
+                            <span className="text-sm text-red-500">User ID is required</span>
                         )}
                     </div>
                     <div className="flex flex-col">
                         <label className="text-base font-medium" htmlFor="nickname">
-                            Nickname
+                            Username or Nickname
                         </label>
                         <input
                             type="text"
@@ -67,10 +69,14 @@ const Modal = ({ isModalOpen, setIsModalOpen, user, setUser }) => {
                 {/* save button */}
                 <div className="flex justify-end mt-4">
                     <button
+                        disabled={!userId}
                         onClick={handleSave}
-                        className="px-4 py-2 bg-violet-500 text-white text-base font-medium rounded-md w-auto shadow-sm hover:bg-violet-700"
+                        className={cn("px-4 py-2 text-white text-base font-medium rounded-md w-auto shadow-sm", {
+                            'bg-violet-500 hover:bg-violet-700': userId,
+                            'bg-violet-400': !userId
+                        })}
                     >
-                        Save
+                        Login
                     </button>
                 </div>
             </div>
